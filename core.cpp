ced1cd00c0ec7711e117d3364ddbfd1a46fdaf03
Mohamad Nizam <mohamadnizam137@outlook.com>
2021-04-22 02:03:53 +0700

    menambah int a, b, c

--- core.cpp
+++ core.cpp
@@ -1,7 +1,16 @@
-#include <iostreram>
+#include <iostream>
 using namespace std;
 int main()
 {
-    cout << "Halo nama saya Mohamad Nizam";
+    cout << "Halo nama saya Mohamad Nizam" << endl;
+
+    int a, b, c;
+    a = 2;
+    b = 3;
+    c = a +b;
+
+    cout << c; 
+
+
     return 0;
 }
